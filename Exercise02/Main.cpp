fe07e3a85f14e10013b6e02a705021bff217ccb2
nansu0425 <nansu0425@gmail.com>
2024-10-17 14:09:15 +0900

    Complete Exercise02
    다양한 타입의 인자를 받아서 공통 타입의 배열로 만드는 함수 템플릿 구현

---
+++ Exercise02/Main.cpp
@@ -0,0 +1,25 @@
+﻿#include <array>
+#include <iostream>
+#include <type_traits>
+#include <utility>
+
+template<typename... Args>
+auto buildArray(Args&&... args) -> std::array<typename std::common_type<Args...>::type, sizeof... (args)>
+{
+	using CommonType = typename std::common_type<Args...>::type;
+
+	return {std::forward<CommonType>((Args&&)args)...};
+}
+
+int main()
+{
+	auto array = buildArray(1, 0u, 'a', 3.2f, false);
+	// auto array = buildArray(1, "Packet", 2.0); // 하나의 공통 타입으로 변환할 수 없어 오류 발생
+
+	for (const auto& e : array)
+	{
+		std::cout << e << " ";
+	}
+
+	return 0;
+}
