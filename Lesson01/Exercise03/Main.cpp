3a5f093cd2c3ba5535c72654584e70f0c5acb51e
nansu0425 <nansu0425@gmail.com>
2024-10-19 17:17:01 +0900

    Complete Exercise 03
    연결 리스트에서 remove_if 함수를 이용한 조건부 원소 삭제

---
+++ Lesson01/Exercise03/Main.cpp
@@ -0,0 +1,59 @@
+﻿#include <string>
+#include <iostream>
+#include <forward_list>
+
+struct Citizen
+{
+	std::string		name;
+	int				age;
+};
+
+std::ostream& operator<<(std::ostream& os, const Citizen& citizen)
+{
+	os << "[" << citizen.name << ", " << citizen.age << "]";
+
+	return os;
+}
+
+int main()
+{
+	std::forward_list<Citizen> citizens =
+	{
+		{"Kim", 22}, {"Lee", 25}, {"Park", 18}, {"Jin", 16}
+	};
+
+	auto copyCitizens = citizens;
+
+	std::cout << "전체 시민들: ";
+	for (const Citizen& c : citizens)
+	{
+		std::cout << c << " ";
+	}
+	std::cout << std::endl;
+
+	citizens.remove_if([](const Citizen& c)
+					   {
+						   return (c.age < 19);
+					   });
+
+	std::cout << "투표권이 있는 시민들: ";
+	for (const Citizen& c : citizens)
+	{
+		std::cout << c << " ";
+	}
+	std::cout << std::endl;
+
+	copyCitizens.remove_if([](const Citizen& c)
+						   {
+							   return (c.age != 18);
+						   });
+
+	std::cout << "내년에 투표권이 생기는 시민들: ";
+	for (const Citizen& c : copyCitizens)
+	{
+		std::cout << c << " ";
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
