239bbd4bc919853be73acf5f5041d5e8c9d82264
nansu0425 <nansu0425@gmail.com>
2024-10-22 19:27:18 +0900

    실습 문제 1: 음악 재생 목록 구현하기
    원형 연결 리스트와 반복자 구현

---
+++ Lesson01/Activity01/CircularLinkedList.h
@@ -0,0 +1,199 @@
+﻿#pragma once
+
+#include <iostream>
+#include <initializer_list>
+
+template<typename T>
+struct Node
+{
+	T		data;
+	Node*	pPrev = nullptr;
+	Node*	pNext = nullptr;
+};
+
+template<typename T>
+class CircularLinkedList
+{
+private:
+	using NodePtr = Node<T>*;
+
+public:
+	class Iterator
+	{
+	public:
+		Iterator(NodePtr pHead, NodePtr pIter)
+			: m_pHead(pHead)
+			, m_pIter(pIter)
+		{}
+
+		T& operator*() { return m_pIter->data; }
+		const T& operator*() const { return m_pIter->data; }
+
+		Iterator& operator++() 
+		{
+			if (m_pIter->pNext == m_pHead)
+			{
+				m_pIter = nullptr;
+			}
+			else
+			{
+				m_pIter = m_pIter->pNext;
+			}
+
+			return *this;
+		}
+
+		Iterator operator++(int)
+		{
+			Iterator prev = *this;
+
+			if (m_pIter->pNext == m_pHead)
+			{
+				m_pIter = nullptr;
+			}
+			else
+			{
+				m_pIter = m_pIter->pNext;
+			}			
+			
+			return prev;
+		}
+
+		bool operator==(const Iterator& other) { return (m_pIter == other.m_pIter); }
+		bool operator!=(const Iterator& other) { return (m_pIter != other.m_pIter); }
+
+		bool isHead() const { return (m_pHead == m_pIter); }
+		NodePtr getPtr() const { return m_pIter; }
+
+	private:
+		NodePtr		m_pHead = nullptr;
+		NodePtr		m_pIter = nullptr;
+	};
+
+	CircularLinkedList() = default;
+
+	CircularLinkedList(const std::initializer_list<T>& list)
+	{
+		for (auto iter = std::rbegin(list); iter != std::rend(list); ++iter)
+		{
+			pushFront(*iter);
+		}
+	}
+
+	~CircularLinkedList()
+	{
+		while (!isEmpty())
+		{
+			popFront();
+		}
+	}
+
+	void pushFront(const T& data)
+	{
+		NodePtr pNode = new Node<T>{data};
+
+		if (isEmpty())
+		{
+			pNode->pNext = pNode;
+			pNode->pPrev = pNode;
+			m_pHead = pNode;
+
+			return;
+		}
+
+		pNode->pNext = m_pHead;
+		pNode->pPrev = m_pHead->pPrev;
+
+		m_pHead->pPrev = pNode;
+		pNode->pPrev->pNext = pNode;
+
+		m_pHead = pNode;
+	}
+
+	void insert(const Iterator& iter, const T& data)
+	{
+		if (iter.isHead())
+		{
+			pushFront(data);
+			return;
+		}
+
+		NodePtr pNextInsert = iter.getPtr();
+		NodePtr pInsert = new Node<T>{data, pNextInsert->pPrev, pNextInsert};
+
+		pNextInsert->pPrev = pInsert;
+		pInsert->pPrev->pNext = pInsert;
+	}
+
+	void remove(const Iterator& iter)
+	{
+		if (iter.isHead())
+		{
+			popFront();
+			return;
+		}
+
+		NodePtr pRemove = iter.getPtr();
+		pRemove->pPrev->pNext = pRemove->pNext;
+		pRemove->pNext->pPrev = pRemove->pPrev;
+
+		delete pRemove;
+	}
+
+	void popFront()
+	{
+		if (isEmpty())
+		{
+			return;
+		}
+
+		if (m_pHead->pNext == m_pHead)
+		{
+			delete m_pHead;
+			m_pHead = nullptr;
+			return;
+		}
+
+		m_pHead->pNext->pPrev = m_pHead->pPrev;
+		m_pHead->pPrev->pNext = m_pHead->pNext;
+
+		NodePtr pDelete = m_pHead;
+		m_pHead = m_pHead->pNext;
+		delete pDelete;
+	}
+
+	bool isEmpty() const { return (m_pHead == nullptr); }
+
+	Iterator begin() const { return Iterator(m_pHead, m_pHead); }
+	Iterator end() const { return Iterator(m_pHead, nullptr); }
+
+	Iterator find(const T& data)
+	{
+		Iterator iter = begin();
+
+		while (iter != end())
+		{
+			if (*iter == data)
+			{
+				break;
+			}
+
+			++iter;
+		}
+
+		return iter;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const CircularLinkedList& list)
+	{
+		for (const T& data : list)
+		{
+			os << data << ' ';
+		}
+
+		return os;
+	}
+
+private:
+	NodePtr		m_pHead = nullptr;
+};
---
+++ Lesson01/Activity01/Main.cpp
@@ -0,0 +1,15 @@
+﻿#include "CircularLinkedList.h"
+
+int main()
+{
+	CircularLinkedList<int> list = {0, 1, 2, 3, 4};
+
+	list.insert(list.begin(), -1);
+	list.insert(list.find(3), 2);
+	list.remove(list.find(1));
+	list.remove(list.find(0));
+
+	std::cout << list << '\n';
+
+	return 0;
+}
