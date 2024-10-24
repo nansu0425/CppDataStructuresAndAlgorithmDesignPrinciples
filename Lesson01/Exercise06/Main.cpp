67ff44c9e8fc827237158efc61a8f4c3debda545
nansu0425 <nansu0425@gmail.com>
2024-10-24 10:43:04 +0900

    std::list의 삽입 또는 삭제 함수 사용하기
    완료

---
+++ Lesson01/Exercise06/Main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+
+int main()
+{
+	std::list<int> list1 = {1, 2, 3, 4, 5};
+	list1.emplace_back(6);
+
+	auto iter = list1.begin();
+	list1.insert(iter, 0);
+
+	iter = list1.end();
+	list1.insert(iter, 7);
+
+	list1.pop_back();
+
+	// std::advance(iter, 1); end iterator를 넘어갈 수 없다
+	std::advance(iter, -3);
+
+	return 0;
+}
